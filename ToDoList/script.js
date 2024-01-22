3d5077b88ce1c05f8983aa3206c6e526d10bd029
infinity <scudarty@gmail.com>
2024-01-22 22:20:31 +0100

    add listener to buttons

--- ToDoList/script.js
+++ ToDoList/script.js
@@ -4,9 +4,13 @@ function createPage (toDoItems) {
     const root = document.getElementById('root')
     root.appendChild(createForm())
 
+    const itemContainer = document.createElement('div')
+    itemContainer.setAttribute('id', 'item_container')
+    root.appendChild(itemContainer)
+
     toDoItems.forEach((element) => {
-        const item = createItem(element)
-        root.appendChild(item)
+        const item = createItem(element.toDoText, element.date)
+        itemContainer.appendChild(item)
     })
 }
 
@@ -18,7 +22,11 @@ function createForm() {
 
     const deleteButton = document.createElement('button')
     deleteButton.innerHTML = 'Delete All'
-    deleteButton.setAttribute('type', 'button')
+    deleteButton.setAttribute('type', 'reset')
+    deleteButton.onclick = function() {
+        const itemContainer = document.getElementById('item_container')
+        itemContainer.innerHTML = ''
+    }
     form.appendChild(deleteButton)
 
     const input = document.createElement('input')
@@ -27,34 +35,65 @@ function createForm() {
 
     const addButton = document.createElement('button')
     addButton.innerHTML = 'Add'
-    addButton.setAttribute('type', 'button')
+    addButton.setAttribute('type', 'reset')
+    addButton.onclick = function() {
+        let objectDate = new Date()
+        let day = objectDate.getDate()
+        let month = objectDate.getMonth() + 1
+        if (month < 10) {
+            month = '0' + month
+        }
+        const fullDate = day + "." + month
+        
+        const newItem = createItem(input.value, fullDate)
+        const itemContainer = document.getElementById('item_container')
+        itemContainer.appendChild(newItem)
+    }
     form.appendChild(addButton)
 
     return form
 }
 
-function createItem (element) {
+function createItem (text, date) {
     const item = document.createElement('div')
     item.className = 'item'
+
 
     const itemBottomCross = document.createElement('button')
     itemBottomCross.className = 'item__button-cross'
     itemBottomCross.innerHTML = '&#215;'
+    itemBottomCross.onclick = function() {
+        console.log('Delete local')
+        const itemContainer = document.getElementById('item_container')
+        itemContainer.removeChild(item)
+    }
     item.appendChild(itemBottomCross)
 
     const itemBottomMark = document.createElement('button')
     itemBottomMark.className = 'item__button-mark'
     itemBottomMark.innerHTML = '&#10003;'
+    itemBottomMark.onclick = function() {
+        console.log('Modify CSS')
+
+        if (itemText.className === 'item__text-modified') {
+            console.log('White text')
+            itemText.className = 'item__text'
+        } else {
+            console.log('Red text and crossed')
+            itemText.className = 'item__text-modified'
+        }
+    }
     item.appendChild(itemBottomMark)
 
     const itemText = document.createElement('p')
     itemText.className = 'item__text'
-    itemText.innerHTML = element.toDoText
+    itemText.setAttribute('id', 'item_text')
+    itemText.innerHTML = text
     item.appendChild(itemText)
 
     const itemDate = document.createElement('p')
     itemDate.className = 'item__date'
-    itemDate.innerHTML = element.date
+    itemDate.innerHTML = date
     item.appendChild(itemDate)
 
     return item
